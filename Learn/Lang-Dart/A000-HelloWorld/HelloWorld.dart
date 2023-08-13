7cd912dbdd4079fdb262dd1216bebe02b97f0e6c
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-08-13 06:48:56 -0700

    Update HelloWorld.dart

--- Learn/Lang-Dart/A000-HelloWorld/HelloWorld.dart
+++ Learn/Lang-Dart/A000-HelloWorld/HelloWorld.dart
@@ -1,3 +1,18 @@
-void Main() {
-  print("Hello World !!");
+void main() {
+  print("Hello World !! - Main");
+  print(K_HELLOWORLD);
+  print(str_helloWorld);
+  Do_HelloWorld();
+  Do_HelloWorldInline();
+  print(Get_HelloWorld());
+}
+
+const String K_HELLOWORLD = "Hello World !! - Const Variable";
+String str_helloWorld = "Hello World !! - Variable";
+String Get_HelloWorld() {
+  return "Hello World !! - Get Function";
+}
+void Do_HelloWorld() => print("Hello World !! - Function");
+void Do_HelloWorldInline({String param_helloWorld = "Hello World !! - Function Param"}) { 
+  print(param_helloWorld);
 }
