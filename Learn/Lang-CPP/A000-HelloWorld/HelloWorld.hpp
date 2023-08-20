61255d3da4599a590b25f8690b4573bae06448ef
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-08-19 18:27:30 -0700

    Update HelloWorld.hpp

--- Learn/Lang-CPP/A000-HelloWorld/HelloWorld.hpp
+++ Learn/Lang-CPP/A000-HelloWorld/HelloWorld.hpp
@@ -10,6 +10,7 @@ class HelloWorld {
 public:
     void Call_HelloWorld() { std::cout << "Hello World !! - Function Run" << std::endl; } 
     std::string Get_strHelloWorld() { return "Hello World from me !! - Function String"; }
+    std::string Get_strHelloWorld02() { return str_helloWorld02; }
 private:
     std::string str_helloWorld02 = "Hello World";
 };
